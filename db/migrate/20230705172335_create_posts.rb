e98b18f08f4fd20b5cdfec10d6e88f2f7515ba38
Anya Parekh <anya.parekh@viget.com>
2023-07-06 12:25:05 -0400

    Squash migrations into CreateActivities and CreatePosts + modify columns

--- db/migrate/20230705172335_create_posts.rb
+++ db/migrate/20230705172335_create_posts.rb
@@ -1,6 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
+      t.string :title, null: false
+      t.text :description, null: false
+      t.string :tags, array: true, default: []
       t.timestamps
     end
   end
