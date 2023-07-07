2d6bf532aa16a59e450361ebdd75b49c03f3cb51
Anya Parekh <anya.parekh@viget.com>
2023-07-07 16:15:01 -0400

    Make tag options accessible to all posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+    TAG_OPTIONS = { 'Eat': 1, 'Entertain': 2, 'Relax': 3, 'Shop': 4, 'Learn': 5, 'Create': 6, 'Exercise': 7, 'Outdoors/Nature': 8, 'Free': 9 }
+
     validates :title, length: { maximum: 50 }, presence: true
     validates :city, length: { maximum: 50 }, presence: true
 
