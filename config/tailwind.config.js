2ebd0c050fe9d20f78294e46eb13eec3b940b496
Robert Koch <robert_koch@brown.edu>
2023-07-10 15:53:47 -0400

    add custom colors to twconfig and parts kit

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -9,8 +9,26 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        form: '#031830',
+        lavender: {
+          500: '#D195FE',
+          700: '#BC86E5',
+          900: '#A777CB',
+          100: '#7D5998'
+        },
+        
+      },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        headline: ['Niramit', 'sans-serif'],
+        body: ['Mulish', 'sans-serif'],
+        sans: ['Mulish', 'sans-serif'],
+      },
+      borderRadius: {
+        'smd': '0.25rem',
       },
     },
   },
