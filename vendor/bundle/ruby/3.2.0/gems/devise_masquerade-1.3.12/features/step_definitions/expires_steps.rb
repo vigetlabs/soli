43b06e8df0e8a35a6b9cabfd78f632ba5b57f0db
Anya Parekh <anya.parekh@viget.com>
2023-07-11 11:57:41 -0400

    rm -rf vendor

--- vendor/bundle/ruby/3.2.0/gems/devise_masquerade-1.3.12/features/step_definitions/expires_steps.rb
+++
@@ -1,9 +0,0 @@
-When("I have devise masquerade expiration time in {int} second") do |seconds|
-  Devise.masquerade_expires_in = seconds.second
-end
-
-Then("I waited for {int} seconds") do |seconds|
-  sleep(seconds)
-
-  Devise.masquerade_expires_in = 5.minutes
-end
--- vendor/bundle/ruby/3.2.0/gems/hotwire-rails-0.1.3/lib/hotwire/engine.rb
+++
@@ -1,4 +0,0 @@
-module Hotwire
-  class Engine < ::Rails::Engine
-  end
-end
