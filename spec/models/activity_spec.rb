ca9487a41fc833a3a7da4997ac6832a860496a9a
Anya Parekh <anya.parekh@viget.com>
2023-07-11 14:08:46 -0400

    Address PR comments

--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -2,51 +2,33 @@ require 'rails_helper'
 
 RSpec.describe Activity, type: :model do
   describe "Validations" do
-    let(:guide) {
-      Guide.create(title: "Test Guide", 
-                city: "Falls Church")
-    }
-    subject {
-      described_class.new(title: "Test Activity", 
-                          location: "Falls Church, VA",
-                          hours_to_complete: 2,
-                          minutes_to_complete: 30,
-                          prompt: "When I was here, I felt...",
-                          prompt_answer: "like I was testing something.",
-                          guide_id: guide.id)
-    }
+    subject { create(:activity) }
     it "is valid with valid attributes" do
       expect(subject).to be_valid
     end
 
     it "is not valid without a title" do
-      subject.title = nil
-      expect(subject).to_not be_valid
+      should validate_presence_of(:title)
     end
 
     it "is not valid without hours to complete" do
-      subject.hours_to_complete = nil
-      expect(subject).to_not be_valid
+      should validate_presence_of(:hours_to_complete)
     end
     
     it "is not valid without minutes to complete" do
-      subject.minutes_to_complete = nil
-      expect(subject).to_not be_valid
+      should validate_presence_of(:minutes_to_complete)
     end
 
     it "is not valid without location" do
-      subject.location = nil
-      expect(subject).to_not be_valid
+      should validate_presence_of(:location)
     end
 
     it "is not valid without prompt" do
-      subject.prompt = nil
-      expect(subject).to_not be_valid
+      should validate_presence_of(:prompt)
     end
 
     it "is not valid without prompt answer" do
-      subject.prompt_answer = nil
-      expect(subject).to_not be_valid
+      should validate_presence_of(:prompt_answer)
     end
 
     it "is not valid without guide id" do
--- spec/models/guide_spec.rb
+++ spec/models/guide_spec.rb
@@ -2,23 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Guide, type: :model do
   describe "Validations" do
-    subject {
-      described_class.new(title: "Test Guide", 
-                          city: "Falls Church")
-    }
+    subject { create(:guide) }
 
     it "is valid with valid attributes" do
       expect(subject).to be_valid
     end
 
     it "is not valid without a title" do
-      subject.title = nil
-      expect(subject).to_not be_valid
+      should validate_presence_of(:title)
     end
 
     it "is not valid without a city" do
-      subject.city = nil
-      expect(subject).to_not be_valid
+      should validate_presence_of(:city)
     end
   end
 end
