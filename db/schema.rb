5f188bc86e60f9bfdaf9da7d120f1c4d795f099f
Anya Parekh <anya.parekh@viget.com>
2023-07-14 10:24:47 -0400

    Add devise gem and migrations

--- db/schema.rb
+++ db/schema.rb
@@ -63,6 +63,20 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_17_134428) do
     t.string "city", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "username", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "activities", "guides"
