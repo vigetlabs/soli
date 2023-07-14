717fd453e6348997b4002ddae4fe5768087247aa
Anya Parekh <anya.parekh@viget.com>
2023-07-14 09:16:30 -0400

    Remove erroring validation

--- app/models/guide.rb
+++ app/models/guide.rb
@@ -8,7 +8,6 @@ class Guide < ApplicationRecord
     validates :title, length: { maximum: 50 }, presence: true
     validates :city, length: { maximum: 50 }, presence: true
     validates :tags, length: { maximum: 3 }
-    validates_length_of :activities, minimum: 1
 
     has_many :activities
     has_one_attached :image
