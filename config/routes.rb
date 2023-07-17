deb58cc22165e90a53937a6f627d2982892e3a40
Anya Parekh <anya.parekh@viget.com>
2023-07-17 16:38:06 -0400

    WIP

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users, only: [:show]
   resources :parts_kit, only: [:index]
   resources :guides, only: [:new, :create, :show, :update, :index] do
     member do
