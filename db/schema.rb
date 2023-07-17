287ef02f8183cbdad9a40849b9bf55698368bbed
Anya Parekh <anya.parekh@viget.com>
2023-07-17 16:00:13 -0400

    Add guides association to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_17_134428) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_17_190503) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,6 +61,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_17_134428) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "city", null: false
+    t.bigint "author_id"
+    t.index ["author_id"], name: "index_guides_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -80,4 +82,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_17_134428) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "activities", "guides"
+  add_foreign_key "guides", "users", column: "author_id"
 end
