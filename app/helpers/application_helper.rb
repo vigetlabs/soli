7e3f6ac8134f1f778c1d036e65647fbdcda2db2a
Anya Parekh <anya.parekh@viget.com>
2023-08-01 10:57:21 -0400

    Refactor hide navbar logic into helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+    def hide_navbar
+        (params[:controller] != 'guides' || params[:action] != 'add_tags') && (params[:controller] != 'guides' || params[:action] != 'show')
+    end
 end
