fd638cd46188ddfd712d9d0b52104acb01086dbe
Robert Koch <robert_koch@brown.edu>
2023-08-01 11:14:56 -0400

    in progress

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,11 +1,18 @@
 class ActivitiesController < ApplicationController
     before_action :authenticate_user!
 
-    def create 
+    def create
         @guide = Guide.find(params[:guide_id])
         @activity = @guide.activities.create(activity_params.except(:hours_to_complete))
+        if @activity.save
+            redirect_to @guide
+        else
+            puts @activity.errors.full_messages
+            # binding.irb
+            @errors = @activity.errors.full_messages
+            redirect_to guide_path(@guide), status: :unprocessable_entity
+        end
         @activity.update!({ minutes_to_complete: @activity.minutes_to_complete + activity_params[:hours_to_complete].to_i * 60 })
-        redirect_to @guide
     end
 
     private
